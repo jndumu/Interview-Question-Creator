39aee45e778fe3dca4faae3cec93c525bdbaf224
josie <latinue@gmail.com>
2024-06-04 11:24:11 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Projects',
+    packages=find_packages(),
+    version='0.0.0',
+    author_email='latinue@gmail.com',
+    author='Josephine Ndumu',
+    install_requires = []
+)
